b6b3a9f02f9bff4c5d0b31b3a0510c12a6191794
AndreyU76 <43966723+AndreyU76@users.noreply.github.com>
2018-10-21 10:29:03 +0300

    Create easy5.2.py

---
+++ easy5.2.py
@@ -0,0 +1,11 @@
+import os
+ 
+os.rmdir("path1")
+os.rmdir("path2")
+os.rmdir("path3")
+os.rmdir("path4")
+os.rmdir("path5")
+os.rmdir("path6")
+os.rmdir("path7")
+os.rmdir("path8")
+os.rmdir("path9")
