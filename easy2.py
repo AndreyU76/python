039b320f9ce2948c509ad84c58b5a548beebfcf0
AndreyU76 <43966723+AndreyU76@users.noreply.github.com>
2018-10-21 09:44:52 +0300

    Delete easy2.py

--- easy2.py
+++
@@ -1,6 +0,0 @@
-#Удовиченко Андрей
-
-fruit_list = ['банан', 'апельсин', 'яблоко', 'груша']
-another_list = ['киви', 'банан', 'персик', 'яблоко']
-sort_list = list (set (fruit_list) & set (another_list))
-print(sort_list)
