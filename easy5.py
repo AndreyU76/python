5b9903cad186954444092a41b1d787384c110097
AndreyU76 <43966723+AndreyU76@users.noreply.github.com>
2018-10-21 10:22:28 +0300

    Update easy5.py

--- easy5.py
+++ easy5.py
@@ -1,3 +1,8 @@
 from subprocess import call
-call(['mkdir', '-p', 'path1/path3'])
+call(['mkdir', '-p', 'path1/'])
 call(['mkdir', '-p', 'path2/'])
+call(['mkdir', '-p', 'path3/'])
+call(['mkdir', '-p', 'path4/'])
+call(['mkdir', '-p', 'path5/'])
+call(['mkdir', '-p', 'path6/'])
+call(['mkdir', '-p', 'path7/'])
