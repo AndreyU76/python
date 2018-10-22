9e6a35a201ae39d897657b18b0e1e1acbdaa7e93
AndreyU76 <43966723+AndreyU76@users.noreply.github.com>
2018-10-22 12:39:29 +0300

    Delete easy5.py

--- easy5.py
+++
@@ -1,8 +0,0 @@
-from subprocess import call
-call(['mkdir', '-p', 'path1/'])
-call(['mkdir', '-p', 'path2/'])
-call(['mkdir', '-p', 'path3/'])
-call(['mkdir', '-p', 'path4/'])
-call(['mkdir', '-p', 'path5/'])
-call(['mkdir', '-p', 'path6/'])
-call(['mkdir', '-p', 'path7/'])
